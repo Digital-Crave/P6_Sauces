9abb7407795a8e792b29b7880b2760fcddcfe0e6
Julien Beral <julien.beraldevaulx@gmail.com>
2022-03-15 14:34:45 +0100

    finish

--- index.js
+++ index.js
@@ -13,16 +13,9 @@ require('./mongo')
 
 app.use(bodyParser.json());
 
-//eviter les erreurs de CORS
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
-    next();
-});
-
-app.get("/", (req, res) => res.send("Hello World!"))
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
 path.join(__dirname)
 app.use("/images", express.static(path.join(__dirname, 'images')))
